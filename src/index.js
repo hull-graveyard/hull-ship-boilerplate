5232d15801abdcb1aa1c17e7df5d095d8aab2845
Romain Dardour <romain@hull.io>
2015-03-11 15:55:42 +0100

    update boilerplate

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import App from './app';
-Hull.init(hullConfig);
 
 var appInit = function(hull, me, platform, org){
-
-  // // When embedded by Hull. this is how the app will be booted automatically:
-  hull.embed(platform.deployments)
-
-  // In development, skip the embed process and start the ship directly
+  // When developing on localhost, skip the embed process and start the ship directly
   // App.start(document.getElementById('ship'),platform.deployments[0]);
+
+  // When embedded by Hull. this is how the app will be booted automatically.
+  // You don't have to call this yourself. Hull will do it for you
+  // hull.embed(platform.deployments)
 }
 
 Hull.ready(appInit)
